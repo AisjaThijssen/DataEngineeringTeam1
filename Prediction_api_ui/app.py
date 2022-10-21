7202a62d8809011b6d0e4a9b861ccebdc5fe46fd
AisjaThijssen <112868481+AisjaThijssen@users.noreply.github.com>
2022-10-21 11:12:49 +0200

    Update app.py

--- Prediction_api_ui/app.py
+++ Prediction_api_ui/app.py
@@ -22,4 +22,4 @@ def result():
       return render_template("prediction.html",result = result)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
